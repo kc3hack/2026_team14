7ced6fb33e8606af746f7ef5403f27281dd95576
yuuma893 <yma9283@gmail.com>
2026-02-22 15:00:06 +0900

    fix

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR /"statics" /
+STATIC_ROOT = BASE_DIR /"static"
 STATICFILES_DIRS = [
     BASE_DIR / "geodjango" / "static",
 ]
